44573096a001e66b3ca260663b8a07b337f9824c
Vlad-Andrei Cojocariu <vlad-andrei.cojocariu@ibm.com>
2025-01-18 20:28:58 +0200

    validparantheses

---
+++ solutions-sorted-by-difficulty/easy/20validparentheses.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isValid(self, s):
+        openParanthesesStack = []
+        mapOfClosingBrackets = {')': '(', ']': '[', '}': '{'}
+        for char in s:
+            if char in mapOfClosingBrackets.values():
+                openParanthesesStack.append(char)
+            elif char in mapOfClosingBrackets.keys():
+                if not openParanthesesStack or openParanthesesStack[-1] != mapOfClosingBrackets[char]:
+                    return False
+                openParanthesesStack.pop()
+            else:
+                return False
+        return not openParanthesesStack
+
+    @staticmethod
+    def run():
+        s = "( { [   )"  # Input string
+        solution = Solution()
+        result = solution.isValid(s)
+        print(f"Is the string '{s}' valid? {result}")
+
+
+# Run the program
+Solution.run()
